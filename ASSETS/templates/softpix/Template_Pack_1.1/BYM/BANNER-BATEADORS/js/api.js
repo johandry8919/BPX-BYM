f075dccecdf0b83f865c84ac1c294fcedc875ef7
johandry8919 <rojas.rojas440@gmail.com>
2023-10-25 15:24:48 -0400

    commit

--- ASSETS/templates/softpix/Template_Pack_1.1/BYM/BANNER-BATEADORS/js/api.js
+++ ASSETS/templates/softpix/Template_Pack_1.1/BYM/BANNER-BATEADORS/js/api.js
@@ -26,130 +26,203 @@ const fetchApiData = (url, params) => {
 
 
 function runTemplateUpdate() {
-     if(e('f3')){
-        if(e('f3').innerText == 'none'){
-            e('logoPatrocinio').style="opacity: 0;"
-            e('cont-3').style.display = 'none'
-            document.querySelector('.contcenter').style.left = '20%'
-    
-        }else if (e('f3').innerText){
-            e('logoPatrocinio').src = e('f3').innerText
-           
-    
-        } 
-
-     }
-
-     function ajustarCadena(cadena) {
-      cadena = cadena.replace(/\+/g, ' ');
-      cadena = cadena.replace(/\+/g, ' ');
-      return cadena;
-    }
-    
-    const expresion = htmlDecode(e('f0').innerText);
-    const cadenaAjustada = ajustarCadena(expresion);
+var   id_peloteros;
+var id_equipo_jugado ; 
   
+let bateadores1 = htmlDecode(e('f1').innerText)
+let bateadores2 = htmlDecode(e('f2').innerText)
+let seleciono;
 
-  const params = {
-      id_juego:  `${cadenaAjustada}`,
-  };
-  
-  const url1 = "https://bss.qualitybeisbol.com/api/boxscore";
+if(bateadores1 != ''){
+  seleciono =  bateadores1
+}else if(bateadores2 != ''){
+  seleciono =  bateadores2
+}
 
-  Promise.all([fetchApiData(url1, params)])
-    .then(([result1]) => {
-
-       let { 
-        id_bateador_homeclub,
-        id_bateador_visitante,
-        id_equipo_homeclub,
-        id_equipo_visitante,
-        parte
-
-        } = result1.data.juego;
+gapi.load('client', initClient);
+var SPREADSHEET_ID = '1kSVuoVD2Y7YON3ATTWGHzGCAA-Wz7sGmRFj0jxLizRY';
+var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+var SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
 
 
-        let iquipo_juega;
-        parte == 0 ? iquipo_juega = id_bateador_visitante : iquipo_juega = id_bateador_homeclub
-        const url = new URL(
-          "https://bss.qualitybeisbol.com/api/anual-pelotero-ave"
-      );
-      
-      const params = {
-          "id_bateador": iquipo_juega,
-          "periodo": "TR",
-      };
-      Object.keys(params)
-          .forEach(key => url.searchParams.append(key, params[key]));
-      
-      
-      fetch(url, {
-          method: "GET",
-          headers,
-      }).then(response => response.json())
-      .then(datas => {
-        datas.data.forEach((element, index) => {
-          if(index == 0){
-            if(document.getElementById('fxg')){
-              document.getElementById('fxg').innerHTML = ` 
-         <div>
-           ${element.AVE == null?'0':element.AVE} <span> AVE </span> / </div>
-          <div> ${element.HR== null?'0' :element.HR} <span > HR </span>  / </div>
-            <div>${element.CI== null?'0':element.CI} <span>CI </span>  / </div> 
-             <div> ${element.OPS== null?'0':element.OPS} <span> OPS</span> </div>`;
-    
-           }
-          }
-        })
+function initClient() {
+   gapi.client.init({
+       apiKey: 'AIzaSyBRYaICc8ckdInbL0JuotA-nXGM-gA-z7I',
+       discoveryDocs: DISCOVERY_DOCS,
+       scope: SCOPES
+   }).then(function () {
+       getDataB();
+   }, function (error) {
+       console.log(error);
+   });
+}
 
-      });
+function getDataB() {
+   gapi.client.sheets.spreadsheets.get({
+       spreadsheetId: SPREADSHEET_ID
+   }).then(function (response) {
+       var sheets = response.result.sheets;
+       // sheets es un array de objetos que contiene información sobre cada hoja.
+       for (var i = 0; i < sheets.length; i++) {
+           var sheet = sheets[i];
 
-        let id_jugador;
-        if(parte == 1){
-          id_jugador = result1.data.juego.id_bateador_homeclub
-  
-          logos_equipos.src = Logos_equipos[id_equipo_homeclub].img_url;
-          data_pelotero = result1.data.boxscore.homeclub.peloteros
-          
-          data_pelotero.forEach(element => {
-            if(element.id_pelotero == id_bateador_homeclub){
-              let resultado = parseInt(element.HIT) + parseInt(element.H2) + parseInt(element.H3);
+           var sheetName = sheet.properties.title;
+           if (sheetName === 'bx') {
+            let datos;
+            if(bateadores2 != '') datos = '!K17:M27'
+              else if(bateadores1 != '') datos = '!O17:Q27'
+               gapi.client.sheets.spreadsheets.values.get({
+                   spreadsheetId: SPREADSHEET_ID,
+                   range: sheetName + datos
+               }).then(function (response) {
+                   var data = response.result.values;
+                   if (data && data.length > 0) {
+                       var primeraFila = data[seleciono];
+                       id_peloteros = primeraFila[1] 
+                       id_equipo_jugado = primeraFila[2] 
+                       Nombre.innerText = primeraFila[0];
+                   } 
 
-              if(e('primer_turno')){
-                e('primer_turno').innerHTML = htmlDecode(e('f1').innerText)
-              }else if(document.getElementById('fxt')){
-                document.getElementById('fxt').innerText = `HOY: ${element.VB} - ${resultado}  / ${element.HR} HR / ${element.HIT} H / ${element.SO} K / ${element.CI} CI`;
+                   if(e('f3')){
+                    if(e('f3').innerText == 'none'){
+                        e('logoPatrocinio').style="opacity: 0;"
+                        e('cont-3').style.display = 'none'
+                        document.querySelector('.contcenter').style.left = '20%'
                 
-              }
-              Nombre.innerText = `${element.nombre} ${element.apellido}`;
-            }
-        });
+                    }else if (e('f3').innerText){
+                        e('logoPatrocinio').src = e('f3').innerText
+
+                    } 
+            
+                 }
+            
+                 function ajustarCadena(cadena) {
+                  cadena = cadena.replace(/\+/g, ' ');
+                  cadena = cadena.replace(/\+/g, ' ');
+                  return cadena;
+                }
+                
+                const expresion = htmlDecode(e('f0').innerText);
+                const cadenaAjustada = ajustarCadena(expresion);
+              
+            
+              const params = {
+                  id_juego:  `${cadenaAjustada}`,
+              };
+              
+              const url1 = "https://bss.qualitybeisbol.com/api/boxscore";
+            
+              Promise.all([fetchApiData(url1, params)])
+                .then(([result1]) => {
+            
+                   let { 
+                    id_bateador_homeclub,
+                    id_bateador_visitante,
+                    id_equipo_homeclub,
+                    id_equipo_visitante,
+                    parte
+            
+                    } = result1.data.juego;
+            
+            
+                    let iquipo_juega;
+                    parte == 0 ? iquipo_juega = id_bateador_visitante : iquipo_juega = id_bateador_homeclub
+                    const url = new URL(
+                      "https://bss.qualitybeisbol.com/api/anual-pelotero-ave"
+                  );
+                  
+                  const params = {
+                      "id_bateador": id_peloteros,
+                      "periodo": "TR",
+                  };
+                  Object.keys(params)
+                      .forEach(key => url.searchParams.append(key, params[key]));
+                  
+                  
+                  fetch(url, {
+                      method: "GET",
+                      headers,
+                  }).then(response => response.json())
+                  .then(datas => {
+                    datas.data.forEach((element, index) => {
+                      if(index == 0){
+                        if(document.getElementById('fxg')){
+                          document.getElementById('fxg').innerHTML = ` 
+                     <div>
+                       ${element.AVE == null?'0':element.AVE} <span> AVE </span> / </div>
+                      <div> ${element.HR== null?'0' :element.HR} <span > HR </span>  / </div>
+                        <div>${element.CI== null?'0':element.CI} <span>CI </span>  / </div> 
+                         <div> ${element.OPS== null?'0':element.OPS} <span> OPS</span> </div>`;
+                
+                       }
+                      }
+                    })
+            
+                  });
+            
+                    let id_jugador;
+                    if(parte == 1){
+                      id_jugador = result1.data.juego.id_bateador_homeclub
+              
+                      logos_equipos.src = Logos_equipos[id_equipo_jugado].img_url;
+                      data_pelotero = result1.data.boxscore.homeclub.peloteros
+                      
+                      data_pelotero.forEach(element => {
+                        if(element.id_pelotero == id_bateador_homeclub){
+                          let resultado = parseInt(element.HIT) + parseInt(element.H2) + parseInt(element.H3);
+            
+                          if(e('primer_turno')){
+                            e('primer_turno').innerHTML = htmlDecode(e('f4').innerText)
+                          }else if(document.getElementById('fxt')){
+                            document.getElementById('fxt').innerText = `HOY: ${element.VB} - ${resultado}  / ${element.HR} HR / ${element.HIT} H / ${element.SO} K / ${element.CI} CI`;
+                            
+                          }
+                         
+                        }
+                    });
+            
+            
+                    }else{
+                      id_jugador = result1.data.juego.id_bateador_visitante
+                      logos_equipos.src = Logos_equipos[id_equipo_jugado].img_url;
+                      data_pelotero = result1.data.boxscore.visitante.peloteros
+            
+                      data_pelotero.forEach(element => {   
+                        if(element.id_pelotero == id_bateador_visitante){
+                          let resultado = parseInt(element.HIT) + parseInt(element.H2) + parseInt(element.H3);
+                          if(e('primer_turno')){
+                            e('primer_turno').innerHTML = htmlDecode(e('f4').innerText)
+                          }else if(document.getElementById('fxt')){
+                            document.getElementById('fxt').innerText = `  HOY: ${element.VB} - ${resultado}  / ${element.HR} HR / ${element.HIT} H / ${element.SO} K / ${element.CI} CI`;
+                          }
+                         
+                        }
+                      });     
+                    }
+                     runAnimationIN();
+            
+                })
+                .catch(error => {
+                  console.error("Error en una de las solicitudes:", error);
+                });
 
 
-        }else{
-          id_jugador = result1.data.juego.id_bateador_visitante
-          logos_equipos.src = Logos_equipos[id_equipo_visitante].img_url;
-          data_pelotero = result1.data.boxscore.visitante.peloteros
+               }, function (error) {
+                   console.log(error);
+               });
+           }
+       }
+   }, function (error) {
+       console.log(error);
+   });
 
-          data_pelotero.forEach(element => {   
-            if(element.id_pelotero == id_bateador_visitante){
-              let resultado = parseInt(element.HIT) + parseInt(element.H2) + parseInt(element.H3);
-              if(e('primer_turno')){
-                e('primer_turno').innerHTML = htmlDecode(e('f1').innerText)
-              }else if(document.getElementById('fxt')){
-                document.getElementById('fxt').innerText = `HOY: ${element.VB} - ${resultado}  / ${element.HR} HR / ${element.HIT} H / ${element.SO} K / ${element.CI} CI`;
-              }
-              Nombre.innerText = `${element.nombre} ${element.apellido}`;
-            }
-          });     
-        }
-         runAnimationIN();
 
-    })
-    .catch(error => {
-      console.error("Error en una de las solicitudes:", error);
-    });
+  
+}
+
 
 }
+
+
 
 
